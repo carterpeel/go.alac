33b62839d552c7c5e44be592dd2bacf090193a23
Magnus Hulthén <magnus@computer.se>
2016-10-17 16:36:33 +0200

    Initialize from ftmp string

--- alac.go
+++ alac.go
@@ -3,6 +3,7 @@ package alac
 
 import (
 	"fmt"
+	"strings"
 )
 
 // New alac decoder. Sample size 16, 2 chan!
@@ -29,6 +30,59 @@ func New() (*Alac, error) {
 	return a, nil
 }
 
+// New alac decoder. Sample size 16, 2 chan!
+func NewFromFmtp(fmtp string) (*Alac, error) {
+	a := create_alac(16, 2)
+	if a == nil {
+		return nil, fmt.Errorf("can't create alac. No idea why, though")
+	}
+
+	iv, err := strings2ints(strings.Split(fmtp, " "))
+	if err != nil {
+		return nil, err
+	}
+
+	// fmtp: 96 352 0 16 40 10 14 2 255 0 0 44100
+	a.setinfo_max_samples_per_frame = uint32(iv[1]) // 24: frameLength uint32;
+	a.setinfo_7a = uint8(iv[2])                     // 28: compatibleVersion byte;
+	a.setinfo_sample_size = uint8(iv[3])            // 29: bitDepth byte;
+	a.setinfo_rice_historymult = uint8(iv[4])       // 30: pb byte;
+	a.setinfo_rice_initialhistory = uint8(iv[5])    // 31: mb byte;
+	a.setinfo_rice_kmodifier = uint8(iv[6])         // 32: kb byte;
+	a.setinfo_7f = uint8(iv[7])                     // 33:  numChannels byte;
+	a.setinfo_80 = uint16(iv[8])                    // 34: maxRun uint16;
+	a.setinfo_82 = uint32(iv[9])                    // 36: maxFrameBytes uint32;
+	a.setinfo_86 = uint32(iv[10])                   // 40: avgBitRate uint32;
+	a.setinfo_8a_rate = uint32(iv[11])              // 44: sampleRate uint32;
+
+	a.allocateBuffers()
+	return a, nil
+}
+
+/*
+Decode a frame.
+*/
 func (a *Alac) Decode(f []byte) []byte {
 	return a.decodeFrame(f)
+}
+
+/*
+Return the number of bits per sample and channel.
+*/
+func (a *Alac) BitDepth() int {
+	return int(a.samplesize)
+}
+
+/*
+Return the number of channels to decode.
+*/
+func (a *Alac) NumChannels() int {
+	return int(a.setinfo_7f)
+}
+
+/*
+Return the SampleRate in samples per second.
+*/
+func (a *Alac) SampleRate() int {
+	return int(a.setinfo_8a_rate)
 }
