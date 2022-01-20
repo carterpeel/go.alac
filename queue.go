07f600fb449051917aae1777ab539c8a2907d3c4
carterpeel <carterpeel@gmail.com>
2022-01-20 11:30:17 -0500

    Added decoder pre-init

--- queue.go
+++ queue.go
@@ -4,29 +4,34 @@ import (
 	"github.com/carterpeel/bobcaygeon/rtsp"
 	kyoo "github.com/dirkaholic/kyoo"
 	"github.com/dirkaholic/kyoo/job"
-	"github.com/sirupsen/logrus"
 )
 
 type AudioQueue struct {
 	finishedChan chan []byte
 	callback     func(data []byte)
 	pool         *kyoo.JobQueue
+	decoders     []*Alac
+	maxDecoders  int
 }
 
 func NewAudioQueue(maxDecoders int, callback func(data []byte)) (aq *AudioQueue) {
 	aq = &AudioQueue{
 		finishedChan: make(chan []byte),
 		callback:     callback,
+		decoders:     make([]*Alac, maxDecoders),
+		maxDecoders:  maxDecoders,
 	}
 
 	aq.pool = kyoo.NewJobQueue(maxDecoders)
 	aq.pool.Start()
 
+	for i := range aq.decoders {
+		aq.decoders[i], _ = New()
+	}
+
 	go func() {
 		for d := range aq.finishedChan {
-			logrus.Infoln("Executing callback")
 			aq.callback(d)
-			logrus.Infoln("Callback complete")
 		}
 	}()
 
@@ -34,16 +39,20 @@ func NewAudioQueue(maxDecoders int, callback func(data []byte)) (aq *AudioQueue)
 }
 
 func (aq *AudioQueue) ProcessSession(session *rtsp.Session) {
+	var decoderOffset int
 	for d := range session.DataChan {
 		aq.pool.Submit(&job.FuncExecutorJob{
 			Func: func() error {
-				decoder, _ := New()
+				offset := decoderOffset
+				decoder := aq.decoders[offset]
 				aq.finishedChan <- decoder.decodeFrame(d)
-				logrus.Infoln("Job finished")
 				return nil
 			},
 		})
-		logrus.Infoln("Submitted job to queue")
+		decoderOffset++
+		if decoderOffset >= aq.maxDecoders {
+			decoderOffset = 0
+		}
 	}
 }
 
